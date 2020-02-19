3720f014a89fb43a3f972182bf009b3abfa6ac2c
Rafi <Rafi@DESKTOP-IQ66E26>
2020-02-19 12:07:05 +0200

    js files removal

--- src/server/src/common/decorators/user.decorator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var common_1 = require("@nestjs/common");
-exports.User = common_1.createParamDecorator(function (field, req) {
-    var _a, _b;
-    if (!((_b = (_a = req.session) === null || _a === void 0 ? void 0 : _a.passport) === null || _b === void 0 ? void 0 : _b.user))
-        throw new common_1.InternalServerErrorException("Couldn't fetch user information.");
-    return field ? req.session.passport.user[field] : req.session.passport.user;
-});
